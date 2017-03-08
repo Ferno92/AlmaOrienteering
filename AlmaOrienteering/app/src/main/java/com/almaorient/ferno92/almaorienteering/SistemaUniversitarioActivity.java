f46782c742c1328d671151316cf20d4ca05dede7
TURBO\ale96 <alessandrolotti1996@gmail.com>
2017-03-08 23:54:01 +0100

    Prova intent, ferno attendo una spiegazione informatica

---
+++ AlmaOrienteering/app/src/main/java/com/almaorient/ferno92/almaorienteering/SistemaUniversitarioActivity.java
@@ -0,0 +1,13 @@
+package com.almaorient.ferno92.almaorienteering;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SistemaUniversitarioActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sistema_universitario_activity);
+    }
+}
