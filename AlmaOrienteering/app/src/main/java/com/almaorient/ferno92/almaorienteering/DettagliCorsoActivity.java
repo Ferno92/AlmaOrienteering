7542635283a126a11d3c7ed9e063561b955e9ebc
TURBO\ale96 <alessandrolotti1996@gmail.com>
2017-03-13 21:52:21 +0100

    Aggiunta intent da elenco scuole a corso

--- AlmaOrienteering/app/src/main/java/com/almaorient/ferno92/almaorienteering/DettagliCorsoActivity.java
+++ AlmaOrienteering/app/src/main/java/com/almaorient/ferno92/almaorienteering/DettagliCorsoActivity.java
@@ -2,6 +2,7 @@ package com.almaorient.ferno92.almaorienteering;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class DettagliCorsoActivity extends AppCompatActivity {
 
@@ -9,5 +10,10 @@ public class DettagliCorsoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dettagli_corso);
+
+        TextView nomecorsoText = (TextView) findViewById(R.id.nomecorso);
+
+        String datopassato = getIntent().getExtras().getString("Vocecliccata");
+        nomecorsoText.setText(datopassato);
     }
 }
