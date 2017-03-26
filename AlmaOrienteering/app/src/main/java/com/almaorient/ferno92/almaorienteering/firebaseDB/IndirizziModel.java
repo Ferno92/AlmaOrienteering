a890df26079aafded28d151a7a1fc0fc1575341b
Alessandro Lotti <Alessandro Lotti>
2017-03-26 16:48:50 +0200

    Tentativi poco riusciti mappe

--- AlmaOrienteering/app/src/main/java/com/almaorient/ferno92/almaorienteering/firebaseDB/IndirizziModel.java
+++ AlmaOrienteering/app/src/main/java/com/almaorient/ferno92/almaorienteering/firebaseDB/IndirizziModel.java
@@ -6,21 +6,12 @@ package com.almaorient.ferno92.almaorienteering.firebaseDB;
 
 public class IndirizziModel {
     public String mCodice;
-    public String mNome;
-    public String mIndirizzo;
-    public String mPiano;
     public String mLatitudine;
     public String mLongitudine;
 
-    public IndirizziModel(){
-        // Default constructor required for calls to DataSnapshot.getValue(User.class)
-    }
 
-    public IndirizziModel(String codice, String nome,String indirizzo, String piano,String latitudine, String longitudine){
+    public IndirizziModel(String codice, String latitudine, String longitudine){
         this.mCodice = codice;
-        this.mNome = nome;
-        this.mIndirizzo = indirizzo;
-        this.mPiano = piano;
         this.mLatitudine = latitudine;
         this.mLongitudine = longitudine;
     }
@@ -29,22 +20,17 @@ public class IndirizziModel {
         return this.mCodice;
     }
 
-    public String getNome(){
-        return this.mNome;
-    }
 
-    public String getIndirizzo(){
-        return this.mIndirizzo;
-    }
-
-    public String getPiano(){
-        return this.mPiano;
-    }
     public String getLatitudine(){
         return this.mLatitudine;
     }
     public String getLongitudine(){
         return this.mLongitudine;
     }
+
+//    public String toStringa ()
+//    {
+//        return this.mCodice;
+//    }
 }
 
